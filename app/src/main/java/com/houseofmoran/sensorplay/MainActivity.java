d7ee67b680d663b5cd52083acd4d8a2b599dfaa3
Mike Moran <mike@houseofmoran.com>
2017-04-13 17:50:45 +0200

    logs sensor values

--- app/src/main/java/com/houseofmoran/sensorplay/MainActivity.java
+++ app/src/main/java/com/houseofmoran/sensorplay/MainActivity.java
@@ -1,13 +1,50 @@
 package com.houseofmoran.sensorplay;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.hardware.TriggerEventListener;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SensorManager mSensorManager;
+    private Sensor mMagneticSensor;
+    private final MagneticSensorListener mListener = new MagneticSensorListener();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mMagneticSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+    }
+
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(mListener, mMagneticSensor, 1000 * 1000);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(mListener);
+    }
+
+    private class MagneticSensorListener implements SensorEventListener {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            Log.i(this.getClass().getName(), Arrays.toString(event.values));
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
     }
 }
