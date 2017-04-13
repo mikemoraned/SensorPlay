dc6de59abb818be8f3b112c4b02356931ceade8a
Mike Moran <mike@houseofmoran.com>
2017-04-13 18:57:18 +0200

    refactor: moved most of listening details into NorthCrossingDeterminer

--- app/src/main/java/com/houseofmoran/sensorplay/MainActivity.java
+++ app/src/main/java/com/houseofmoran/sensorplay/MainActivity.java
@@ -8,47 +8,48 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-import java.util.Arrays;
-
-import static android.R.attr.orientation;
-
 public class MainActivity extends AppCompatActivity {
 
-    private SensorManager mSensorManager;
-    private Sensor mMagneticSensor;
-    private Sensor mAccelSensor;
-    private final BearingDeterminer mListener = new BearingDeterminer();
+    private NorthCrossingDeterminer mListener = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        mMagneticSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
-        mAccelSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        SensorManager mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        Sensor mMagneticSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        Sensor mAccelSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        mListener = new NorthCrossingDeterminer(mSensorManager, mMagneticSensor, mAccelSensor);
     }
 
     protected void onResume() {
         super.onResume();
-
-        int samplingPeriodUs = 1000 * 1000;
-        mSensorManager.registerListener(mListener, mMagneticSensor, samplingPeriodUs);
-        mSensorManager.registerListener(mListener, mAccelSensor, samplingPeriodUs);
+        mListener.startListening();
     }
 
     protected void onPause() {
         super.onPause();
-        mSensorManager.unregisterListener(mListener, mMagneticSensor);
-        mSensorManager.unregisterListener(mListener, mAccelSensor);
+        mListener.stopListening();
     }
 
-    private class BearingDeterminer implements SensorEventListener {
+    private class NorthCrossingDeterminer implements SensorEventListener {
+        private SensorManager mSensorManager;
+        private Sensor mMagneticSensor;
+        private Sensor mAccelSensor;
+
         private float[] mLastAccel = null;
         private float[] mLastMagnetic = null;
 
         private static final float UNSET_AZIMUTH = Float.MIN_VALUE;
         private float mLastAzimuth = UNSET_AZIMUTH;
+
+        public NorthCrossingDeterminer(SensorManager mSensorManager, Sensor mMagneticSensor, Sensor mAccelSensor) {
+            this.mSensorManager = mSensorManager;
+            this.mMagneticSensor = mMagneticSensor;
+            this.mAccelSensor = mAccelSensor;
+        }
 
         @Override
         public void onSensorChanged(SensorEvent event) {
@@ -100,6 +101,17 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onAccuracyChanged(Sensor sensor, int accuracy) {
             // ignore
+        }
+
+        public void startListening() {
+            int samplingPeriodUs = 1000 * 1000;
+            mSensorManager.registerListener(this, mMagneticSensor, samplingPeriodUs);
+            mSensorManager.registerListener(this, mAccelSensor, samplingPeriodUs);
+        }
+
+        public void stopListening() {
+            mSensorManager.unregisterListener(this, mMagneticSensor);
+            mSensorManager.unregisterListener(this, mAccelSensor);
         }
     }
 }
